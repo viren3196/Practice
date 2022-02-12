2daed3ed1c0c637bce57be2184c1131f4eba4932
Virendra <virendrasehwag3196@gmail.com>
2022-02-12 10:26:40 +0530

    Time: 90 ms (60.51%), Space: 85.8 MB (76.42%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.java
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -1,17 +1,33 @@
 class SeatManager {
     PriorityQueue<Integer> pq;
+    //List<Integer> avlSeats;
+    // int[] heap;
+    // int size;
     public SeatManager(int n) {
         pq = new PriorityQueue<>(n);
+        // avlSeats = new ArrayList<>();
         for(int j=1; j<=n ; j++)
             pq.add(j);
     }
     
+    //3 4 5 
+    
     public int reserve() {
         return pq.remove();
+        // int temp = avlSeats.get(0);
+        // avlSeats.remove(0);
+        // return temp;
     }
     
     public void unreserve(int seatNumber) {
         pq.add(seatNumber);
+        // int i;
+        // for(i=0 ; i<avlSeats.size() ; i++){
+        //     if(seatNumber<avlSeats.get(i)){
+        //         break;
+        //     }    
+        // }
+        // avlSeats.add(i,seatNumber);
     }
 }
 
